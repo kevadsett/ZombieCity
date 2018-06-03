3afeb2d3d9e7fd50d5b3fdd14dc4cc8df67ba69c
Mark Gossage <mark.gossage@bossalien.com>
2018-06-03 12:24:46 +0100

    day night controller

---
+++ Unity/Assets/Scripts/DayNightUi.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayNightUi : MonoBehaviour
+{
+	private Image dayNightImg;
+	private RectTransform dayNightRT;
+	private DayNightController controller;
+	private Text text;
+
+	// Use this for initialization
+	void Start ()
+	{
+		controller = GameObject.FindObjectOfType<DayNightController>();
+		text = GetComponentInChildren<Text>();
+		dayNightImg = transform.Find("Mask").Find("DayNight").GetComponent<Image>();
+		dayNightRT = dayNightImg.GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		dayNightRT.rotation=Quaternion.Euler(0,0,controller.Days * -360);
+		int day = Mathf.FloorToInt(controller.Days);
+		float delta = controller.Days - day;
+		if (delta < 0.5f)
+		{
+			text.text = string.Format("Day {0}", day);
+		}
+		else
+		{
+			text.text = string.Format("Night {0}", day);
+		}
+	}
+}
--- Unity/Assets/Scripts/GameSettings.cs
+++ Unity/Assets/Scripts/GameSettings.cs
@@ -7,4 +7,5 @@ using UnityEngine;
 public class GameSettings : ScriptableObject 
 {
     public float vignetteTime=2.0f;
+    public float dayDuration = 60.0f;
 }
