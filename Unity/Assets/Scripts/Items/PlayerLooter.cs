864ee7f2bcd55a9f881c24e7b3072c2cc6412579
Gavin Jones <gavin@bossalien.com>
2018-06-03 10:59:08 +0100

    first bit of looty code, not really done anything yet tbh

---
+++ Unity/Assets/Scripts/Items/PlayerLooter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLooter : MonoBehaviour {
+	[SerializeField] KeyCode lootButton;
+	[SerializeField] float maxLootRadius;
+
+	void Update () {
+		if (Input.GetKeyDown (lootButton)) {
+			
+		}
+	}
+}
