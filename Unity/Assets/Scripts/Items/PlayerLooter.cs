db90427eb26291f57224130e67e03e9db3255a7b
Gavin Jones <gavin@bossalien.com>
2018-06-03 11:42:25 +0100

    looting is closer to being a thing now

--- Unity/Assets/Scripts/Items/PlayerLooter.cs
+++ Unity/Assets/Scripts/Items/PlayerLooter.cs
@@ -5,10 +5,39 @@ using UnityEngine;
 public class PlayerLooter : MonoBehaviour {
 	[SerializeField] KeyCode lootButton;
 	[SerializeField] float maxLootRadius;
+	[SerializeField] GameObject pressFireToLootText;
+	[SerializeField] GameObject alreadyLootedText;
 
 	void Update () {
-		if (Input.GetKeyDown (lootButton)) {
-			
+		Vector3 targetDir = Camera.main.transform.forward;
+		Vector3 sourcePos = Camera.main.transform.position;
+
+		RaycastHit hit;
+		if (Physics.Raycast(sourcePos, targetDir, out hit))
+		{
+			pressFireToLootText.SetActive (false);
+			alreadyLootedText.SetActive (false);
+
+			for (int i = 0; i < CityGenerator.numBuildings; i++) {
+				Building biru = CityGenerator.GetBuilding (i);
+
+				Collider doorCollider = biru.doorCollider;
+
+				if (hit.collider == doorCollider && Vector3.Distance (doorCollider.transform.position, sourcePos) < maxLootRadius) {
+
+					if (biru.hasBeenLooted) {
+						alreadyLootedText.SetActive (true);
+					} else {
+						pressFireToLootText.SetActive (true);
+					}
+
+					if (Input.GetKeyDown (lootButton)) {
+						Debug.LogWarning ("LOOTING SHOULD HAPPEN NOW");
+
+						biru.hasBeenLooted = true;
+					}
+				}
+			}
 		}
 	}
 }
