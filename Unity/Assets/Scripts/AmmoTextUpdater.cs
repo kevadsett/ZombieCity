6a572a0b8d425d2810c285804b188e073f0ad208
Kev Adsett <kev.adsett@gmail.com>
2018-06-03 13:53:08 +0100

    Ammo on screen

---
+++ Unity/Assets/Scripts/AmmoTextUpdater.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class AmmoTextUpdater : MonoBehaviour
+{
+	private WeaponSettings weaponSettings;
+	private Text text;
+
+	// Use this for initialization
+	void Start()
+	{
+		text = GetComponent<Text>();
+		WeaponSelector.OnWeaponChanged += WeaponSelectorOnOnWeaponChanged;
+		BulletRaycaster.OnShotsFired += BulletRaycasterOnOnShotsFired;
+	}
+
+	private void BulletRaycasterOnOnShotsFired(List<Vector3> positions)
+	{
+		UpdateText();
+	}
+
+	private void OnDestroy()
+	{
+		WeaponSelector.OnWeaponChanged -= WeaponSelectorOnOnWeaponChanged;
+		BulletRaycaster.OnShotsFired -= BulletRaycasterOnOnShotsFired;
+	}
+
+	private void WeaponSelectorOnOnWeaponChanged(WeaponSettings newWeapon)
+	{
+		weaponSettings = newWeapon;
+		UpdateText();
+	}
+
+	private void UpdateText()
+	{
+		text.text = WeaponStorage.Instance.Ammo[weaponSettings.Name].ToString();
+	}
+}
