8bc2acb357b9d22bc33f47473f16ca871ed1aad4
Mark Gossage <mark.gossage@bossalien.com>
2018-06-03 10:10:15 +0100

    zombie model, renamed the settings object

---
+++ Unity/Assets/Scripts/GameSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Data", menuName = "Settings/Game", order = 2)]
+public class GameSettings : ScriptableObject 
+{
+    public float vignetteTime=2.0f;
+}
---
+++ Unity/Assets/Scripts/ZombieSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Settings/Zombie", order = 1)]
+public class ZombieSettings : ScriptableObject
+{
+    [Header("Detection/Movement")] 
+    public float sightDistance = 10;
+    public float lostDistance=30;
+    public float chaseSpeed=2,turnSpeed=30;
+
+    [Header("Attack")]
+    [Tooltip("range at which the zombie attacks")]
+    public float attackRange = 1.0f;
+    [Tooltip("Amount of time for attack animation")]
+    public float attackTime = 1.0f;
+    [Tooltip("time within the animation that the damage is applied")]
+    public float attackDamageAt = 1.0f;
+    public float playerKnockback = 100, zombieKnockback = 50;
+    public float deathDuration = 1f;
+}
