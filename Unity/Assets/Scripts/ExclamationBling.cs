e27e7f17cf2d74bb84c13efb3ff7f563b75730e8
Gavin Jones <gavin@bossalien.com>
2018-06-05 20:23:31 +0100

    added a dash of pizzazz to zombies noticing you (via exclamation mark animating over their heads)

---
+++ Unity/Assets/Scripts/ExclamationBling.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExclamationBling : MonoBehaviour {
+	[SerializeField] float animationRate;
+	[SerializeField] AnimationCurve scaleCurve;
+
+	float timer;
+	Vector3 defaultScale;
+
+	void Awake () {
+		defaultScale = transform.localScale;
+	}
+
+	void Update () {
+		transform.localScale = defaultScale * (1f + scaleCurve.Evaluate (timer));
+		timer += Time.deltaTime * animationRate;
+	}
+
+	void OnEnable () {
+		timer = 0f;
+	}
+}
